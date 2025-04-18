888e3a2e061683eebfee7a016384a9aae6b2d83c
kandis7 <m.wuerfele@ok.de>
2025-04-18 22:03:29 +0200

    active section removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,11 @@ import { useInformationStore } from "@/lib/store"
 import InformationPage from "./information/page"
 
 export default function HomePage() {
-  const activeSection = useInformationStore((state) => state.activeSection)
-
   return (
     <div className="prose dark:prose-invert">
-      {!activeSection && (
-        <>
-          <h1>Welcome to TRAI</h1>
-          <p>Select a section from the sidebar to begin.</p>
-        </>
-      )}
-
-      {activeSection === "information" && <InformationPage />}
+      <h1>Welcome to TRAI</h1>
+      <p>Select a section from the sidebar to begin.</p>
     </div>
   )
+  
 }

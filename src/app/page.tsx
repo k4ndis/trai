9eb5bd37b6895320550a385d6a7495d4d6755892
kandis7 <m.wuerfele@ok.de>
2025-04-18 22:23:09 +0200

    imports deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 // src/app/page.tsx
 "use client"
 
-import { useInformationStore } from "@/lib/store"
-import InformationPage from "./information/page"
-
 export default function HomePage() {
   return (
     <div className="prose dark:prose-invert">

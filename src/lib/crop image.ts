e9ebcb464508c6a2b8aa387e76554a9198ef059c
k4ndis <m.wuerfele@web.de>
2025-04-21 22:45:19 +0200

    Create crop image.ts

---
+++ src/lib/crop image.ts
@@ -0,0 +1,29 @@
+export async function getCroppedImg(imageSrc: string, pixelCrop: any): Promise<Blob> {
+  const image = new Image()
+  image.src = imageSrc
+  await new Promise((resolve) => (image.onload = resolve))
+
+  const canvas = document.createElement("canvas")
+  canvas.width = pixelCrop.width
+  canvas.height = pixelCrop.height
+  const ctx = canvas.getContext("2d")!
+
+  ctx.drawImage(
+    image,
+    pixelCrop.x,
+    pixelCrop.y,
+    pixelCrop.width,
+    pixelCrop.height,
+    0,
+    0,
+    pixelCrop.width,
+    pixelCrop.height
+  )
+
+  return await new Promise((resolve) => {
+    canvas.toBlob((blob) => {
+      if (!blob) throw new Error("Canvas blob ist leer.")
+      resolve(blob)
+    }, "image/jpeg")
+  })
+}

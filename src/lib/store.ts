e0eee484b85806abdad8c0769f69407526aaacc3
k4ndis <m.wuerfele@web.de>
2025-04-24 07:16:56 +0200

    Update store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -18,7 +18,7 @@ export interface TestSequence {
   temperature: string
   dwelltime: string
   comment: string
-  sampleIds: (number | string)[]
+  sampleIds: number []
 }
 
 export interface SampleImage {

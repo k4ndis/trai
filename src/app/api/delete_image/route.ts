fd15a75a90efc324c1dddf46dede203e756a9d4f
kandis7 <m.wuerfele@ok.de>
2025-04-27 20:20:01 +0200

    secret_roley_key_supabase

--- src/app/api/delete_image/route.ts
+++ src/app/api/delete_image/route.ts
@@ -1,6 +1,11 @@
 // src/app/api/delete-image/route.ts
+import { createClient } from "@supabase/supabase-js"
 import { NextRequest, NextResponse } from "next/server"
-import { supabase } from "@/lib/supabaseClient"
+
+const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+)
 
 export async function DELETE(req: NextRequest) {
   const { filePath } = await req.json()
@@ -9,7 +14,7 @@ export async function DELETE(req: NextRequest) {
     return NextResponse.json({ error: "Kein Pfad angegeben" }, { status: 400 })
   }
 
-  const { error } = await supabase.storage.from("trai").remove([filePath])
+  const { error } = await supabaseAdmin.storage.from("trai").remove([filePath])
 
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 500 })

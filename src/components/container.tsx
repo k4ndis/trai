16fca0b94b2ac0bb76a70e30d071aa0fa0833cd0
kandis7 <m.wuerfele@ok.de>
2025-04-28 16:53:38 +0200

    content layout container_sample section2

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,17 +3,18 @@
 
 import { cn } from "@/lib/utils"
 
-interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-export function Container({ className, ...props }: ContainerProps) {
+export function Container({ className, children, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={cn(
         "px-4 md:px-8 lg:px-12 py-4 flex justify-center",
         className
       )}
+      {...props}
     >
-      <div className="w-full max-w-7xl" {...props} />
+      <div className="w-full max-w-7xl">
+        {children}
+      </div>
     </div>
   )
 }

981de19d80eb7746d2a25665220f4a2169d9dac2
kandis7 <m.wuerfele@ok.de>
2025-04-30 20:06:21 +0200

    supabase sample section_18

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        github: "border bg-muted hover:bg-accent text-foreground shadow-sm px-4 py-1.5 rounded-md",
+        github: "bg-primary text-white hover:bg-primary/90 shadow-sm px-4 py-1.5 rounded-sm",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",

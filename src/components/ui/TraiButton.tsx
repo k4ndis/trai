54dc667dc45d311cefca8c21f38b762a33797f3a
kandis7 <m.wuerfele@ok.de>
2025-05-01 13:32:31 +0200

    MUI_intergation_samplesection_5

--- src/components/ui/TraiButton.tsx
+++ src/components/ui/TraiButton.tsx
@@ -3,13 +3,17 @@
 import Button from "@mui/material/Button"
 import type { ButtonProps } from "@mui/material/Button"
 
-export default function TraiButton(props: ButtonProps) {
+export default function TraiButton({ sx, ...props }: ButtonProps) {
   return (
     <Button
       variant="contained"
       color="primary"
       size="small"
-      sx={{ textTransform: "none", borderRadius: 2 }}
+      sx={{
+        textTransform: "none",
+        borderRadius: 2,
+        ...sx, // ⬅ überschreibt gezielt nur, was du beim Aufruf veränderst
+      }}
       {...props}
     />
   )

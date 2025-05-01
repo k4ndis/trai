fc59a457fdccea11a6eeb11b7725f425ed28e578
kandis7 <m.wuerfele@ok.de>
2025-05-01 21:36:06 +0200

    MUI_intergation_samplesection_11

--- src/components/MuiThemeWrapper.tsx
+++ src/components/MuiThemeWrapper.tsx
@@ -1,13 +1,13 @@
 "use client"
 
 import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles"
-import CssBaseline from "@mui/material/CssBaseline"
+
 import muiTheme from "@/lib/mui_theme"
 
 export default function MuiThemeWrapper({ children }: { children: React.ReactNode }) {
   return (
     <MuiThemeProvider theme={muiTheme}>
-      <CssBaseline />
+      
       {children}
     </MuiThemeProvider>
   )

073f68414184059c6c8eaa8a6deaefdf16ce6690
kandis7 <m.wuerfele@ok.de>
2025-05-02 16:24:45 +0200

    sample upload_cropper_6

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -10,6 +10,8 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
+import "@/styles/cropper.css";
+
 
 interface Props {
   sampleId: number

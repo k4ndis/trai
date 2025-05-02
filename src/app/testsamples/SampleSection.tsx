0afbae0265b1a55bb4c047f3e36eed45dbfce407
kandis7 <m.wuerfele@ok.de>
2025-05-02 13:26:56 +0200

    materia react__integration_4

--- src/app/testsamples/SampleSection.tsx
+++ src/app/testsamples/SampleSection.tsx
@@ -1,7 +1,6 @@
 // src/app/testprocedure/SampleSection.tsx
 "use client"
 
-import { Label } from "@/components/ui/label"
 import { Container } from "@/components/container"
 import { SampleTable } from "@/components/SampleTable"
 
